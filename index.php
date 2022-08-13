b85e3b7fe6907061925c6ea4de4675565ceeefd4
chairil0303 <chairilsyahrain24@gmail.com>
2022-08-13 12:06:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,11 @@
 </head>
 <body>
 	<h2>Chairil syahrain</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Itaque ipsum recusandae doloremque, minima voluptate praesentium repudiandae quia unde repellendus vel sunt pariatur temporibus.</p>
+	<ul>
+		<li>anggota 1</li>
+		<li>anggota 2</li>
+		<li>anggota 5</li>
+	</ul>
 </body>
 </html>		
