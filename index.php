6f25aa8b05736a7ae0c4e651702f78321c97d6b4
chairil0303 <chairilsyahrain24@gmail.com>
2022-08-13 12:07:20 +0700

    menambahkan background color

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>coba git remote</title>
+	<style>
+		body{
+			background-color: grey;
+		}
+	</style>
 </head>
 <body>
 	<h2>Chairil syahrain</h2>
